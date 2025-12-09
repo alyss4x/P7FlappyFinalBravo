42c1a2275e3fd31c09a65297c47b8700d2086ae2
alyss4x <726931@student.dusd.net>
2025-12-09 15:29:22 -0800

    created background
    and starting to make the bird jump with code

---
+++ BravoP7FlappyFinal/Assets/Scripts/bird.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+
+    public float upforce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<RigidBody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upforce));
+            }
+        }
+    }
+}
